c004d902098743398bec057785daf2de0e638393
Christopher Cotton <cdc2175@columbia.edu>
2020-12-31 06:58:36 -0500

    Adjusted python preambles

--- country_match.py
+++ country_match.py
@@ -4,57 +4,11 @@ Codes to match countries to their alphanumeric 3 codes.
 Want to work from short official English name to get name in pycountry or directly get alphanumeric 3 codes.
 pycountry matches the countries to name on wikipedia i.e. https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3 .
 """
-# PYTHON_PREAMBLE_START_STANDARD:{{{
-
-# Christopher David Cotton (c)
-# http://www.cdcotton.com
-
-# modules needed for preamble
-import importlib
 import os
 from pathlib import Path
 import sys
 
-# Get full real filename
-__fullrealfile__ = os.path.abspath(__file__)
-
-# Function to get git directory containing this file
-def getprojectdir(filename):
-    curlevel = filename
-    while curlevel is not '/':
-        curlevel = os.path.dirname(curlevel)
-        if os.path.exists(curlevel + '/.git/'):
-            return(curlevel + '/')
-    return(None)
-
-# Directory of project
-__projectdir__ = Path(getprojectdir(__fullrealfile__))
-
-# Function to call functions from files by their absolute path.
-# Imports modules if they've not already been imported
-# First argument is filename, second is function name, third is dictionary containing loaded modules.
-modulesdict = {}
-def importattr(modulefilename, func, modulesdict = modulesdict):
-    # get modulefilename as string to prevent problems in <= python3.5 with pathlib -> os
-    modulefilename = str(modulefilename)
-    # if function in this file
-    if modulefilename == __fullrealfile__:
-        return(eval(func))
-    else:
-        # add file to moduledict if not there already
-        if modulefilename not in modulesdict:
-            # check filename exists
-            if not os.path.isfile(modulefilename):
-                raise Exception('Module not exists: ' + modulefilename + '. Function: ' + func + '. Filename called from: ' + __fullrealfile__ + '.')
-            # add directory to path
-            sys.path.append(os.path.dirname(modulefilename))
-            # actually add module to moduledict
-            modulesdict[modulefilename] = importlib.import_module(''.join(os.path.basename(modulefilename).split('.')[: -1]))
-
-        # get the actual function from the file and return it
-        return(getattr(modulesdict[modulefilename], func))
-
-# PYTHON_PREAMBLE_END:}}}
+__projectdir__ = Path(os.path.dirname(os.path.realpath(__file__)) + '/')
 
 # Convert Alternative Country Name into Short Official English Name OLD:{{{1
 def commonalternatives_toname():
--- large.py
+++ large.py
@@ -9,64 +9,18 @@ Basic idea:
     - close to write last elements and close file.
 
 """
-# PYTHON_PREAMBLE_START_STANDARD:{{{
-
-# Christopher David Cotton (c)
-# http://www.cdcotton.com
-
-# modules needed for preamble
-import importlib
 import os
 from pathlib import Path
 import sys
 
-# Get full real filename
-__fullrealfile__ = os.path.abspath(__file__)
-
-# Function to get git directory containing this file
-def getprojectdir(filename):
-    curlevel = filename
-    while curlevel is not '/':
-        curlevel = os.path.dirname(curlevel)
-        if os.path.exists(curlevel + '/.git/'):
-            return(curlevel + '/')
-    return(None)
-
-# Directory of project
-__projectdir__ = Path(getprojectdir(__fullrealfile__))
-
-# Function to call functions from files by their absolute path.
-# Imports modules if they've not already been imported
-# First argument is filename, second is function name, third is dictionary containing loaded modules.
-modulesdict = {}
-def importattr(modulefilename, func, modulesdict = modulesdict):
-    # get modulefilename as string to prevent problems in <= python3.5 with pathlib -> os
-    modulefilename = str(modulefilename)
-    # if function in this file
-    if modulefilename == __fullrealfile__:
-        return(eval(func))
-    else:
-        # add file to moduledict if not there already
-        if modulefilename not in modulesdict:
-            # check filename exists
-            if not os.path.isfile(modulefilename):
-                raise Exception('Module not exists: ' + modulefilename + '. Function: ' + func + '. Filename called from: ' + __fullrealfile__ + '.')
-            # add directory to path
-            sys.path.append(os.path.dirname(modulefilename))
-            # actually add module to moduledict
-            modulesdict[modulefilename] = importlib.import_module(''.join(os.path.basename(modulefilename).split('.')[: -1]))
-
-        # get the actual function from the file and return it
-        return(getattr(modulesdict[modulefilename], func))
-
-# PYTHON_PREAMBLE_END:}}}
+__projectdir__ = Path(os.path.dirname(os.path.realpath(__file__)) + '/')
 
 def yieldsplitfromchunks(filename, maxbytes=2000000000, encoding='UTF-8', split = '\n', removelastnewline = False, gzipfile = False):
     """
     Read chunks and split them into lines as I go. Advantage over read whole file is doesn't exceed memory capacity. Advantage over read line by line is doesn't require too many disk readings.
 
     USAGE:
-    for line in importattr(__projectdir__ / Path('large.py'), 'yieldsplitfromchunks')(wikifolder + 'redirect/current/csv/output.csv', maxbytes = 1000000000, encoding='latin-1'):
+    for line in yieldsplitfromchunks(wikifolder + 'redirect/current/csv/output.csv', maxbytes = 1000000000, encoding='latin-1'):
     """
 
     if gzipfile is True:
@@ -104,6 +58,6 @@ def yieldsplitfromchunks(filename, maxbytes=2000000000, encoding='UTF-8', split 
             for line in lines:
                 yield(line)
 
-# for line in importattr(__projectdir__ / Path('large.py'), 'yieldsplitfromchunks')('hello.txt', maxbytes = 10000, encoding='latin-1', split = '\n\n\n\n'):
+# for line in yieldsplitfromchunks('hello.txt', maxbytes = 10000, encoding='latin-1', split = '\n\n\n\n'):
 #     print(line)
 #     print(1)
